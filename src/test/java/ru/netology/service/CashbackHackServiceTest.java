1b41085c93965a1c1b898423a9335116a020a25c
Sofya <sofa33158@gmail.com>
2023-04-11 19:54:20 +0300

    ErrorJunit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,60 +1,58 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+public class CashbackHackServiceTest extends TestCase {
 
-public class CashbackHackServiceTest {
 
-    CashbackHackService service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
 
-    @Test
-    public void amountLess1000() {
+        @Test
+        public void amountLess1000() {
 
-        int actual = service.remain(536);
-        int expected = 464;
+            int actual = service.remain(536);
+            int expected = 464;
 
-        assertEquals(actual, expected);
+            Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void amountMore1000() {
+
+            int actual = service.remain(1536);
+            int expected = 464;
+
+            Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void amount1000() {
+
+            int actual = service.remain(1000);
+            int expected = 0;
+
+            Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void amountLittleLess1000() {
+
+            int actual = service.remain(999);
+            int expected = 1;
+
+            Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void amountLittleMore1000() {
+
+            int actual = service.remain(2001);
+            int expected = 999;
+
+            Assert.assertEquals(actual, expected);
+        }
+
+
     }
-
-    @Test
-    public void amountMore1000() {
-
-        int actual = service.remain(1536);
-        int expected = 464;
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void amount1000() {
-
-        int actual = service.remain(1000);
-        int expected = 0;
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void amountLittleLess1000() {
-
-        int actual = service.remain(999);
-        int expected = 1;
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void amountLittleMore1000() {
-
-        int actual = service.remain(2001);
-        int expected = 999;
-
-        assertEquals(actual, expected);
-    }
-
-
-
-
-
-}
